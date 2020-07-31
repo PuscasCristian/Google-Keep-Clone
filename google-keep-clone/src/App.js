f3e0e646c18d7558967b332343827e25e6599c61
Puscas Cristian <pusckyh@gmail.com>
2020-07-31 18:39:55 +0300

    Form update

--- google-keep-clone/src/App.js
+++ google-keep-clone/src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {Button, makeStyles, TextField, FormControl, InputLabel, Input} from '@material-ui/core';
 import './App.css';
+import Note from './Note';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  root: {
+    '& .MuiInputBase-root': {
+      margin: theme.spacing(1),
+      width: '500px',
+    },
+  }
+  
+}));
+
+
 
 function App() {
+  const classes = useStyles();
+  const [input, setInput] = useState('');
+  const [notes, setNotes] = useState(['']);
+  const sendNotes = (event) => {
+    event.preventDefault();
+    // all the logic to send a message goes here
+    setNotes([...notes, input]);
+    setInput(''); 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormControl>
+      <TextField id="outlined-basic" label="Take a note" variant="outlined" multiline value={input} onChange={event => setInput(event.target.value) }/>
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.button}
+        onClick={sendNotes}
+        disabled={!input}
+      >
+        Send
+      </Button>
+      </FormControl>
+      {
+        notes.map(note => (
+          <Note text={note}/>
+        ))
+      }
     </div>
   );
 }
